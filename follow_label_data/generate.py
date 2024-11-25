0b746e2acef1940d779da36ffeb875aca26e7fee
penglingwei <penglingwei@zju.edu.cn>
2024-11-25 10:20:30 +0800

    debug thm args subs

--- follow_label_data/generate.py
+++ follow_label_data/generate.py
@@ -138,8 +138,9 @@ def get_thm_train_data(thm, arg_map={}):
         )
         state = get_block_train_data(new_a_targets, new_a_conditions, new_a_dvs)
         splitted_label = "<label> " + ' '.join(list(label)) + " </label>"
-        args = " ".join([arg + " </arg>" for arg in args])
-        memory = " ".join([state, splitted_label, args]) 
+        subs_args = stmt_subs(args, [], [], arg_map)[0]
+        subs_args = " ".join([arg + " </arg>" for arg in subs_args])
+        memory = " ".join([state, splitted_label, subs_args]) 
         memories.append(tokenizer(memory))
 
     new_operators = []
